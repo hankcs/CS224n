28eb26382dd783c51aa2fd1c7d6013fc87c955e6
Stark_JC <401661801@qq.com>
2018-10-14 17:33:13 +0800

    print func 2.7->3.6

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -46,21 +46,21 @@ def test_sigmoid_basic():
     Some simple tests to get you started.
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print("Running basic tests...")
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
-    print f
+    print(f)
     f_ans = np.array([
         [0.73105858, 0.88079708],
         [0.26894142, 0.11920292]])
     assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
-    print g
+    print(g)
     g_ans = np.array([
         [0.19661193, 0.10499359],
         [0.19661193, 0.10499359]])
     assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
-    print "You should verify these results by hand!\n"
+    print("You should verify these results by hand!\n")
 
 
 def test_sigmoid():
@@ -70,7 +70,7 @@ def test_sigmoid():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print("Running your tests...")
     ### YOUR CODE HERE
     # raise NotImplementedError
     ### END YOUR CODE
